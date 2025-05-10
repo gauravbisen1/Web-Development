1e332d533df027d55893b2bf0ea8b0d84c9014a2
gauravbisen1 <gouravbisen123@gmail.com>
2025-05-10 12:16:27 +0530

    begining of JS!

---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+// console.log("Hello World!");
+// console.log("begining..");
+// let a=10;
+// let b = 20;
+// console.log(`The sum is ${a+b}.for purchase click below->`)
+
+// let string = "agaurav";
+// if ((string[0] === 'a')&&(string.length>3)) {
+//     console.log("Good string");
+// }else{
+//     console.log("bad string dont use.")
+// }
+
+// alert("Mock drill!");
+// console.error("mock drill is ongoing.");
+// console.warn("soon it will be end!");
+// console.info("describe");
+
+let fname = prompt("enter your first name:");
+let lname = prompt("enter your last name:");
+let msg = fname + " " + lname + "!";
+alert(msg);
