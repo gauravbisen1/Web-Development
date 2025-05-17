533025e30519d156b5ba92cb22c90e7a75eb67ec
gauravbisen1 <gouravbisen123@gmail.com>
2025-05-17 23:22:45 +0530

    exhausted!

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -20,3 +20,10 @@
 // let lname = prompt("enter your last name:");
 // let msg = fname + " " + lname + "!";
 // alert(msg);
+
+let inp = document.querySelector("#text");
+let p = document.querySelector("p");
+inp.addEventListener("input",function(){
+    console.log(inp.value);
+    p.innerText=inp.value;
+});
