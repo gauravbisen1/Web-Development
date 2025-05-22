8269c1bc9339ca67f0348d3206dfb9dac747f7d2
gauravbisen1 <gouravbisen123@gmail.com>
2025-05-22 16:12:56 +0530

    node js!

---
+++ NodeJS/script.js
@@ -0,0 +1,10 @@
+// let args = process.argv;
+
+// for (let i = 2; i < args.length; i++) {
+//     console.log("hello & welcome to" , args[i]);
+// }
+// console.log("hello??");
+
+const math = require("./math");
+console.log(math);
+console.log(math.sum(2,2));
