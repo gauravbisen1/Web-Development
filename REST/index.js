af4a757bba4ca7ef7b7d77a550ae4daf65d56555
gauravbisen1 <gouravbisen123@gmail.com>
2025-05-25 01:44:06 +0530

    done with RESTful APIs

--- REST/index.js
+++ REST/index.js
@@ -3,6 +3,18 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.listen(port, ()=>{
+    console.log(`Listening to port ${port}`);
+})
+
+//require method override
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+//require uuid package
+const { v4: uuidv4 } = require('uuid');
+//uuidv4();// => '1b9d6cd-bbfd-4b3d-9b5d-ab8dfbbd4bed'
+
 //to make data to understand to express
 app.use(express.urlencoded({extended:true}));
 
@@ -12,24 +24,22 @@ app.set("view engine", "ejs");//view engine set to ejs
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static( path.join(__dirname, "public")));
 
-app.listen(port, ()=>{
-    console.log(`Listening to port ${port}`);
-})
+
 
 //play like database to store data and do crud operations
 let posts = [
     {
-        id : "1a",
+        id: uuidv4(),
         username: "Gaurav",
         content: "I love coding!"
     },
     {
-        id : "2b",
+        id: uuidv4(),
         username: "Tony",
         content: "I am IronMan!"
     },
     {
-        id : "3c",
+        id: uuidv4(),//if space before colon it consider is name 
         username: "Thor",
         content: "God of thunder!"
     }
@@ -47,8 +57,12 @@ app.get("/posts/new", (req,res)=>{
 
 app.post("/posts", (req,res)=>{
     let {username,content} = req.body;//destructuring
+
+    //creating new id too
+    let id = uuidv4();
+
     //to create new post on posts array
-    posts.push({username,content});
+    posts.push({username,content,id});
     res.redirect("/posts");
 });
 
@@ -56,6 +70,34 @@ app.post("/posts", (req,res)=>{
 app.get("/posts/:id", (req,res)=>{
     let {id} = req.params;
     //now find post of id in posts array
-    let post = post.find((p)=> id===p.id);
+    let post = posts.find((p) => id === p.id);//find in posts array
     res.render("show.ejs", {post});
 });
+
+//update route - /posts/:id
+app.patch("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content=newContent;
+    console.log(post);
+    res.redirect("/posts");
+
+});
+//edit route - /posts/:id/edit
+app.get("/posts/:id/edit", (req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs",{post});//sending post info{}
+});
+
+
+//destroy route - /posts/:id
+app.delete(" /posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    
+    res.redirect("/posts");
+});
+
+
