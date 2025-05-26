de88fcde05bf63f3211523ef513fd7ef9508ccb5
gauravbisen1 <gouravbisen123@gmail.com>
2025-05-26 23:17:16 +0530

    starting of express app

--- SQL_with_node/index.js
+++ SQL_with_node/index.js
@@ -9,9 +9,29 @@ const connection = mysql.createConnection({
     password : "1234",
 });
 
+
+//store 100 user data (fake)
+let getRandomUser = ()=>{
+    return [
+        faker.string.uuid(),
+        faker.internet.username(),
+        faker.internet.email(),
+        faker.internet.password(),
+    ];
+};
+
+//Inserting new data
+let q = "INSERT INTO user (id,username,email,password) VALUES ?";
+
+//to get 100 user data
+let data =[];
+for (let i = 1; i <=100 ; i++) {
+    data.push(getRandomUser());
+}
+
 //to use
 try {
-    connection.query("SHOW TABLES", (err,result)=>{
+    connection.query(q,[data], (err,result)=>{
         if(err) throw err;
         console.log(result);
     })
