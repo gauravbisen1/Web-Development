6ae40be7fed48854c3560c1e50e4cab2e4288ac0
gauravbisen1 <gouravbisen123@gmail.com>
2025-05-30 21:45:25 +0530

    all crud operation complete in chat!

--- MongoDB_Express/index.js
+++ MongoDB_Express/index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.use(express.static(path.join(__dirname,"public")));
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 app.listen(8080,()=>{
     console.log("server is listening on port 8080");
@@ -63,4 +65,21 @@ app.get("/chats/:id/edit",async (req,res)=>{
     let chat = await Chat.findById(id);
     res.render("edit.ejs",{ chat });
 });
+
+//update route
+app.put("/chats/:id", async (req,res)=>{
+    let {id} = req.params;
+    let {msg : newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id , {msg : newMsg} , {runvalidators:true,new:true});
+    console.log(updatedChat);
+    res.redirect("/chats");
+})
+
+//destroy route
+app.delete("/chats/:id", async (req,res)=>{
+    let {id} = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
+})
 
