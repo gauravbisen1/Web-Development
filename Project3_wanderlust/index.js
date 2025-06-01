c77b21aac4cbe168224af075b628fa48c782b55d
gauravbisen1 <gouravbisen123@gmail.com>
2025-06-01 14:32:45 +0530

    styling is ongoing..

--- Project3_wanderlust/index.js
+++ Project3_wanderlust/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
@@ -29,6 +31,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 //testing
 // app.get("/testData", async (req,res)=>{
