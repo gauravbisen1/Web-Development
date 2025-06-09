d28a6e14a03fdf027e19054737cd0c02023318f9
gauravbisen1 <gouravbisen123@gmail.com>
2025-06-09 14:12:19 +0530

    authorization done for review!

--- Project3_wanderlust/middleware.js
+++ Project3_wanderlust/middleware.js
@@ -1,4 +1,5 @@
 const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema,reviewSchema} = require("./schema.js");
 
@@ -53,3 +54,16 @@ module.exports.validateReview = (req,res,next)=>{
         next();
     }
 }; 
+
+
+
+//middleware to check review owner
+module.exports.isReviewAuthor = async (req,res,next) =>{
+    let {id,reviewId} = req.params;
+    let review = await Review.findById(reviewId);
+    if (!review.author.equals(res.locals.currUser._id)) {
+        req.flash("error" , "You are not the author of the review!");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
