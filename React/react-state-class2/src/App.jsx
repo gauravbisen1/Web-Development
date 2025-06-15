b6cad2515a09dc284b4ac24a13fc8d32351030db
gauravbisen1 <gouravbisen123@gmail.com>
2025-06-15 21:54:05 +0530

    state design pattern!

--- React/react-state-class2/src/App.jsx
+++ React/react-state-class2/src/App.jsx
@@ -1,11 +1,16 @@
-import TodoList from './TodoList'
+import Lottery from './Lottery'
 import './App.css'
-
+import { sum } from './helper'
 
 function App() {
+  let winCondition = (ticket) =>{
+    return sum(ticket) === 15; //num sum is 15
+    // return ticket.every((num) => num === ticket[0]); //all num will same
+    // return ticket[0] === 0;
+  }
    return (
     <>
-      <TodoList/>
+      <Lottery n={3} winCondition={winCondition} />
     </>
   )
 }
