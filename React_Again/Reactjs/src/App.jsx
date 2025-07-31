2bbc705749bcded34679c248f25a9b817118b88e
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-01 01:20:12 +0530

    react 01:20am

--- React_Again/Reactjs/src/App.jsx
+++ React_Again/Reactjs/src/App.jsx
@@ -1,24 +1,79 @@
 import React from 'react'
-import axios from 'axios'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Product from './pages/Product'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 
 const App = () => {
-
-  const getData = async ()=>{
-    const response = await axios.get('https://picsum.photos/v2/list');
-    console.log(response);
-  }
-
   return (
-    <div className='p-10'>
-      <button onClick={getData} className='bg-teal-900 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90'>Get Data</button>
-      <div className='p-5 mt-5 bg-gray-800'>
-          hello
-      </div>
+    <div>
+      <Navbar/>
+       <Routes>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/product' element={<Product/>}/>
+       </Routes>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// get data from api
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+
+// const App = () => {
+
+//   const [data, setData] = useState([])
+
+//   const getData = async ()=>{
+//     const response = await axios.get('https://picsum.photos/v2/list');
+//     setData(response.data)
+//     // console.log(data)
+//   }
+
+//   useEffect(() => {
+//     getData()
+//   }, [])
+  
+
+//   return (
+//     <div className='p-10'>
+//       <button onClick={getData} className='bg-teal-900 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90'>Get Data</button>
+//       <div className='p-5 mt-5 bg-gray-800'>
+//           {data.map(function(elem,idx){
+//             return <div key={idx} className='bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3'>
+//               <img className='h-40' src={elem.download_url} alt="" />
+//               <h1>{elem.author}</h1>
+//             </div>
+//           })}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
 
 
 
--- React_Again/Reactjs/src/components/Navbar.jsx
+++ React_Again/Reactjs/src/components/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const 
  Navbar = () => {
+  
   return (
     <nav className='bg-emerald-950 flex py-5 px-10 item-center justify-between'>
-        <h1 className='text-4xl '>Gaurav</h1>
-        <div className='flex gap-10 items-center'>
-          <h4 className='text-2xl'>Home</h4>
-          <h4 className='text-2xl'>contact us</h4>
-          <h4 className='text-2xl'>services</h4>
-          <h4 className='text-2xl'>Account</h4>
+        <h2 className='text-4xl '>Gaurav <input type="text" className='text-black bg-white'/></h2>
+        <div className='flex gap-10 items-center text-lg underline'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+          <Link to='/product'>Product</Link>
+          {/* <a href='/' className='text-2xl'>Home</a>
+          <a href='/contact' className='text-2xl'>Contact us</a>
+          <a href='/product' className='text-2xl'>Product</a>
+          <a href='/about' className='text-2xl'>About</a> */}
         </div>
       </nav>
   )
